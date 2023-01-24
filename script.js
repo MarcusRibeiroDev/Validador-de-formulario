43c8a7bce88c154aec3c5a5c540f66244524a172
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-01-24 20:42:10 -0300

    Campos vazios
    Adicionando validação de campos vazios

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-(function(){
 
+    'use-strict'
     // CRIANDO BLOQUEIO DE ENVIO DE FORMULÁRIO CASO OS CAMPOS NÃO SEJAM PRENCHIDOS CORRETAMENTE
 
     const form = document.querySelector('.validator--form')
@@ -8,12 +8,29 @@
 
     const inputEmail = document.querySelector('#email')
 
-    const inputSubject = document.querySelector('#subject')
+    const inputSubject = document.querySelector('.list')
     
-    let feedbackMessage = document.querySelector('.feedbackMessage')
+    const feedbackMessage = document.querySelector('.feedbackMessage')
 
-    // CONTINUAR 
-    
-})
+    //
+
+    form.addEventListener('submit',function(e){
+        if(!inputUser.value || !inputEmail.value || !inputSubject.value){
+            showError('Existem campos vazios')
+            e.preventDefault()
+        }  
+        // Continuar criando o focus e as outras validações
+    })
+
+    function showError(msg){
+        feedbackMessage.classList.add('show')
+
+        const spanElement = document.querySelector('.span--1')
+        const createdElement = document.createElement('p')
+        createdElement.textContent = msg
+        spanElement.appendChild(createdElement)
+    }
+
+
 
     
