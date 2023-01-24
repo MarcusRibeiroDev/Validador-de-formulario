6ea28d6f5bd95b791fc83711824a0dd20ec066f0
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-01-24 19:25:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Validador-de-formulario
+ Formulário de solicitação de atendimento, usando HTML, CSS e JS puro.
---
+++ script.js
@@ -0,0 +1,19 @@
+(function(){
+
+    // CRIANDO BLOQUEIO DE ENVIO DE FORMULÁRIO CASO OS CAMPOS NÃO SEJAM PRENCHIDOS CORRETAMENTE
+
+    const form = document.querySelector('.validator--form')
+
+    const inputUser = document.querySelector('#user-name')
+
+    const inputEmail = document.querySelector('#email')
+
+    const inputSubject = document.querySelector('#subject')
+    
+    let feedbackMessage = document.querySelector('.feedbackMessage')
+
+    // CONTINUAR 
+    
+})
+
+    
